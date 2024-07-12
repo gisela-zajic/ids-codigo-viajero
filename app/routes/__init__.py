a503ceabf359a4188bf56fdf02cc0cadda9997a1
Gise Zajic <106554970+gisela-zajic@users.noreply.github.com>
2024-07-12 18:27:06 -0300

    Feat: Completing __init__.py from routs

---
+++ app/routes/__init__.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+
+# importo los blueprints de las diferentes rutas
+from .auth_routes import auth_bp
+from .destination_routes import destinos_bp
+from .package_routes import paquetes_turisticos_bp
+from .reservation_routes import reservas_bp
+from .review_routes import resenias_bp
+
+# creo el blueprint principal
+main_bp = Blueprint('main', __name__)
+
+# registro blueprints individuales en el blueprint principal
+main_bp.register_blueprint(auth_bp, url_prefix='/auth')
+main_bp.register_blueprint(destinos_bp, url_prefix='/destinos')
+main_bp.register_blueprint(paquetes_turisticos_bp, url_prefix='/paquetes_turisticos')
+main_bp.register_blueprint(reservas_bp, url_prefix='/reservas')
+main_bp.register_blueprint(resenias_bp, url_prefix='/resenias')
