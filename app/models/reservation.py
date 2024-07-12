bbd4f9e262672e306f02d343ee2384946ae878bb
Gise Zajic <106554970+gisela-zajic@users.noreply.github.com>
2024-07-12 16:46:32 -0300

    Feat: Models Reorganizing information from database in a model folder and a file for each entity

---
+++ app/models/reservation.py
@@ -0,0 +1,11 @@
+import datetime
+from app import db
+
+
+class Reservas(db.Model):
+    __tablename__ = 'reservas'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
+    status = db.Column(db.String(50), default='activa')
+    paquete_id = db.Column(db.Integer, db.ForeignKey('paquetes_turisticos.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
---
+++ app/models/user.py
@@ -0,0 +1,11 @@
+import datetime
+from app import db
+
+
+class Usuarios(db.Model):
+    __tablename__ = 'usuarios'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(30), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
