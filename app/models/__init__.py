e815de04df8bb0ecea65b67a2e3c31731c2245da
Gise Zajic <106554970+gisela-zajic@users.noreply.github.com>
2024-07-14 20:31:25 -0300

    Fix: Change on __init__.py models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,9 @@
-from .destination import Destinos
-from .package import PaquetesTuristicos
-from .reservation import Reservas
-from .review import Resenias
-from .user import Usuarios
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+from app.models.user import Usuarios
+from app.models.destination import Destinos
+from app.models.package import PaquetesTuristicos
+from app.models.reservation import Reservas
+from app.models.review import Resenias
