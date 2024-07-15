58336ed7a5c3a231864b71d4357f3acc955da16e
Gise Zajic <106554970+gisela-zajic@users.noreply.github.com>
2024-07-15 18:31:06 -0300

    Feat:
    Working on tests for auth/users:
    - creating new user --> 200 code

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -14,3 +14,14 @@ def client():
         yield client
         with app.app_context():
             db.drop_all()
+
+
+# prueba de registro exitoso
+def test_register_200(client):
+    response = client.post('/auth/register', json={
+        'username': 'testuser',
+        'email': 'test@example.com',
+        'password': 'password123'
+    })
+    assert response.status_code == 201
+    assert response.json['message'] == 'Usuario creado correctamente como: testuser'
