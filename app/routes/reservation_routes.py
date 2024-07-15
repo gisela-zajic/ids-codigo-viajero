a656d3aab82bdec9cbaac5a1cf77f794391a6103
Gise Zajic <106554970+gisela-zajic@users.noreply.github.com>
2024-07-14 23:04:48 -0300

    Feat:
    Adding method to get a reservation by its id

--- app/routes/reservation_routes.py
+++ app/routes/reservation_routes.py
@@ -5,6 +5,26 @@ from app import db
 reservas_bp = Blueprint('reservas', __name__)
 
 
+# ruta para obtener una reserva por su id
+@reservas_bp.route('/<int:id>', methods=['GET'])
+def get_reserva(id):
+    try:
+        reserva = Reservas.query.get(id)
+        if reserva is None:
+            return jsonify({'message': 'La reserva no fue encontrada'}), 404
+        reserva_data = {
+            'id': reserva.id,
+            'created_at': reserva.created_at,
+            'status': reserva.status,
+            'paquete_id': reserva.paquete_id,
+            'user_id': reserva.user_id,
+        }
+        return jsonify(reserva_data)
+    except Exception as error:
+        print('Error', error)
+        return jsonify({'message': 'Internal server error'}), 500
+
+
 # ruta para obtener todas las reservas
 @reservas_bp.route('/', methods=['GET'])
 def get_reservas():
