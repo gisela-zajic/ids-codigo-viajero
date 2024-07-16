70498987ee9f82b5e5a510898199eb8e7a0e1cd4
Gise Zajic <106554970+gisela-zajic@users.noreply.github.com>
2024-07-16 17:16:43 -0300

    Fix:
    Changing route on config.py from database

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgresql@localhost:5432/codigo_viajero'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgresql@localhost:5434/codigo_viajero'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
