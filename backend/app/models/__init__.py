f93e4d325ffbc8eefd5b0bd43f4bd062854a1644
Gise Zajic <106554970+gisela-zajic@users.noreply.github.com>
2024-07-20 19:52:01 -0300

    Fix: reformatting files
    - Removing unnecessary files due to moving code to existing files

--- backend/app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-from backend.app.models.user import Usuarios
-from backend.app.models.destination import Destinos
-from backend.app.models.package import PaquetesTuristicos
-from backend.app.models.reservation import Reservas
-from backend.app.models.review import Resenias
