300e553491197b38c41fb7a1656dc0ab5c37c8ff
Gise Zajic <106554970+gisela-zajic@users.noreply.github.com>
2024-07-20 19:50:46 -0300

    Fix: reformatting files
    - Changed run.py to app.py file name
    - Moved app creation to app.py file and started app

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_migrate import Migrate
+
+from backend.database.configs.database import db
+from backend.routes.register import register_routes
+
+PORT = 5433
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('backend.database.configs.database.Config')
+
+    db.init_app(app)
+    Migrate(app, db)
+
+    register_routes(app)
+    return app
+
+
+if __name__ == '__main__':
+    print('Starting server...')
+    app = create_app()
+    with app.app_context():
+        db.create_all()
+    print('Started...')
+    app.run(debug=True, port=PORT)
