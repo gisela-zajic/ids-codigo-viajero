378da0073021e580c7f8a925d0869392b55cb4c5
Keren Hasi <khasi@fi.uba.ar>
2024-07-23 14:31:42 +0000

    Fix redirections in index, login

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_migrate import Migrate
-
+from flask_cors import CORS
 from backend.database.configs.database import db
 from backend.routes.register import register_routes
 
@@ -15,6 +15,7 @@ def create_app():
     Migrate(app, db)
 
     register_routes(app)
+    CORS(app)
     return app
 
 
