be0280340997e13cd8bbbc538e1a0c161c469ebd
Keren Hasi <118856545+HasiKeren03@users.noreply.github.com>
2024-07-26 19:19:03 -0300

    Delete backend directory

--- backend/database/configs/database.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgresql@localhost:5434/codigo_viajero'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-db = SQLAlchemy()
